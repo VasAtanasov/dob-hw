d26564e781ede1f8fd60dd7a77b4978cceff3003
Vasil Atanasov <vas.atanasov@gmail.com>
2021-06-03 19:19:27 +0300

    m4 practice

---
+++ practice/m4/assets/provision/bootstrap.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+
+export DEBIAN_FRONTEND="noninteractive"
+export PYTHONUNBUFFERED=1
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+echo $SCRIPT_DIR
+
+export ANSIBLE_CONFIG="${SCRIPT_DIR}/ansible/ansible.cfg"
+
+########################
+# Pre provision provisioning
+########################
+
+# install ansible if needed
+if [ -z "`which ansible-playbook`" ]; then
+   echo " ***************************************************************************** "
+   echo " *** Starting installation of ansible "
+   echo " ***************************************************************************** "
+   apt-get update
+   apt-get -q -y install software-properties-common sshpass
+   add-apt-repository --yes --update ppa:ansible/ansible
+   apt-get -q -y update
+   apt-get -q -y install ansible
+fi
+
+# ########################
+# # Provion with ansible
+# ########################
+
+# echo " ***************************************************************************** "
+# echo " *** Starting provision with ansible (will take some time...)"
+# echo " ***************************************************************************** "
+
+# ANSIBLE_EXTRA_VARS=""
+
+# # fix windows compatiblity
+# cp -a "$SCRIPT_DIR/ansible/inventory" "/tmp/$$.inventory"
+# chmod -x -- "/tmp/$$.inventory"
+
+# # run ansible
+# ansible-playbook "$SCRIPT_DIR/ansible/playbook.yml" --inventory="/tmp/$$.inventory" --extra-vars="$ANSIBLE_EXTRA_VARS" --tags="bootstrap"
+
+# # remove inventory file
+# rm -f -- "/tmp/$$.inventory"
