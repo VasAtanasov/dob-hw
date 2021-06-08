2978ae3a1c5a006f6f794187fd58ee80a1a04269
Vasil Atanasov <Vasil.Atanasov@brandmaker.com>
2021-06-08 13:52:20 +0300

    install jenkins script

--- practice/m5/assets/bootstrap.sh
+++ practice/m5/assets/bootstrap.sh
@@ -8,11 +8,25 @@ set -o errexit   ## set -e : exit the script if any statement returns a non-true
 export DEBIAN_FRONTEND="noninteractive"
 export PYTHONUNBUFFERED=1
 
-username=vagrant
-cp /etc/sudoers /etc/sudoers.bak
-bash -c "echo '${username} ALL=(ALL) NOPASSWD: ALL' | (EDITOR='tee -a' visudo)"
+disable_sudo_password() {
+  local username="${1}"
+  cp /etc/sudoers /etc/sudoers.bak
+  bash -c "echo '${username} ALL=(ALL) NOPASSWD: ALL' | (EDITOR='tee -a' visudo)"
+}
+
+disable_sudo_password("vagrant")
+# disable_sudo_password("jenkisn")
 
 sed -i '/.*PasswordAuthentication.*/d' /etc/ssh/sshd_config
 echo 'PasswordAuthentication yes' | tee -a /etc/ssh/sshd_config > /dev/null
 service sshd restart
 service sshd status
+
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jdk
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+sudo apt-get -y install jenkins
